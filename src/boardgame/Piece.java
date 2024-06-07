a0ad4a9bc7cfdbdb74f66c20ce9af3a2fa69914b
Gabriel Matheus <GabrielMth>
2024-06-07 15:02:45 -0300

    Possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,19 +1,36 @@
 package boardgame;
 
-public class Piece {
-	
+public abstract class Piece {
+
 	protected Position position;
 	private Board board;
-	
-	
+
 	public Piece(Board board) {
 		this.board = board;
-		position = null; // se eu não atribuir nada por padrão o java já atribui mas too colocando para enfatizar
+		position = null; // se eu não atribuir nada por padrão o java já atribui mas too colocando para
+							// enfatizar
 	}
-
 
 	protected Board getBoard() {
 		return board;
 	}
-	
+
+	public abstract boolean[][] possibleMoves();
+
+	public boolean possibleMove(Position position) {
+		return possibleMoves()[position.getRow()][position.getColumn()];
+	}
+
+	public boolean isThereAnyPossibleMove() {
+		boolean[][] mat = possibleMoves();
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat.length; j++) {
+				if (mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
 }
